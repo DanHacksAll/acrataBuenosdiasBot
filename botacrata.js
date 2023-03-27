c4d71c146d0c56171e18c75cf0f625012ecda2e7
Alsec <54718030+DanHacksAll@users.noreply.github.com>
2023-03-27 15:28:21 -0600

    Add files via upload

---
+++ botacrata.js
@@ -0,0 +1,55 @@
+const TelegramBot = require("node-telegram-bot-api");
+
+// Token de acceso de tu bot de Telegram
+const token = "6102270762:AAHR6S6oX4ilFnBBYFWXsKt17AzNnPgdYQI";
+
+// Rutas de las imágenes que deseas enviar para cada día de la semana
+const imagenes = {
+  0: "./img/snapshot.jpg",
+  1: "./img/snapshot.jpg",
+  2: "./img/snapshot.jpg",
+  3: "./img/snapshot.jpg",
+  4: "./img/snapshot.jpg",
+  5: "./img/snapshot.jpg",
+  6: "./img/snapshot.jpg",
+};
+
+// Hora a la que deseas enviar las imágenes
+const horaDeEnvio = "06:00:00"; // Cambia esto por la hora que desees
+
+// Crea un nuevo bot de Telegram
+const bot = new TelegramBot(token, { polling: true });
+
+// Maneja los comandos "/start" y "/help"
+bot.onText(/\/(start|help)/, (msg, match) => {
+  // Guarda el chatId del usuario en una variable global
+  global.chatId = msg.chat.id;
+
+  // Envía un mensaje de bienvenida al usuario
+  bot.sendMessage(global.chatId, "Hola, este es un bot de prueba.");
+
+  // Función que se ejecutará para enviar la imagen correspondiente
+  function enviarImagenDelDia() {
+    // Obtiene el día de la semana actual
+    const diaActual = new Date().getDay();
+
+    // Ruta de la imagen correspondiente al día de la semana actual
+    const imagen = imagenes[diaActual];
+
+    // Envia la imagen al chat
+    bot.sendPhoto(global.chatId, imagen);
+  }
+
+  // Programa la ejecución de la función a la hora especificada cada día
+  setInterval(() => {
+    const ahora = new Date();
+    const horaActual = ahora.getHours();
+    const minutoActual = ahora.getMinutes();
+    const segundoActual = ahora.getSeconds();
+
+    if (horaActual === 06 && minutoActual === 00 && segundoActual === 0) {
+      // Cambia esto por la hora a la que desees enviar las imágenes
+      enviarImagenDelDia();
+    }
+  }, 1000); // Revisa cada segundo si es hora de enviar la imagen
+});
